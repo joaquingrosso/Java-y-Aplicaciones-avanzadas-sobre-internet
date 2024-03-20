d1fccbfeb06119b54b66ec1a674e87a9cd461917
Joaquin Grosso <joagrosso@hotmail.com>
2024-03-19 22:56:34 -0300

    avanzando practica 1

---
+++ Practica1Web40/src/main/java/misservlets/Bienvenida.java
@@ -0,0 +1,53 @@
+package misservlets;
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Bienvenida
+ */
+@WebServlet("/Bienvenida")
+public class Bienvenida extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public Bienvenida() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		String nombre = request.getParameter("nombre");
+
+        
+        response.setContentType("text/html");
+
+        
+        PrintWriter out = response.getWriter();
+        out.println("<html><head><title>Bienvenida</title></head><body>");
+        out.println("<h1>Bienvenido " + nombre + "</h1>");
+        out.println("</body></html>");
+		out.close();
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub		
+		doGet(request, response);
+	}
+
+}
